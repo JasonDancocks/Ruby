a0bf07f3edf3eeaa2372ef67de03bb043b11c52a
Jason Dancocks <jason.dancocks@gmail.com>
2017-07-13 23:04:33 +0100

    Refactored
    Used classes alot more, removed redundant code and general refactor. Need to add some more error checking

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -1,97 +1,13 @@
 require_relative "player"
 
-def set_row
-  row = []
-  n = 0
-  while n < 4
-    puts "Enter peg #{n+1} (RBYGOP)"
-    until valid_input(row,n)
-    end
-    n += 1
-  end
-  return row
-end
-
-def valid_input(arr,n)
-  input = gets.chomp.downcase
-  case input
-  when "r" then arr[n] = "R" ; true
-  when "b" then arr[n] = "B" ; true
-  when "y" then arr[n] = "Y" ; true
-  when "g" then arr[n] = "G" ; true
-  when "o" then arr[n] = "O" ; true
-  when "p" then arr[n] = "P" ; true
-  else
-    puts "Please enter a valid colour (RBYGOP)"
-    false
-  end
-end
-
-
-def matches(current_row,code_array)
-  n = 0
-  matches=[]
-  while n < 4
-    if current_row[n] == code_array[n]
-      matches[n] = "BM"
-    elsif code_array.include?(current_row[n]) 
-      matches[n] = "CM"
-    else 
-      matches[n] = "  "
-    end
-    n += 1
-  end
-  return matches
-end
-
-def print_output(breaker,arr,f_arr)
-  t = 0
-  while t < breaker.turns + 1
-   puts "#{arr[t]} #{f_arr[t]}"
-   t += 1
-  end
-end
-
-
-def check_for_win(breaker,arr,win)
-  if(arr[0] == "BM" && arr[1] == "BM" && arr[2] == "BM" && arr[3] == "BM")
-    puts "You win! You took #{breaker.turns} attempts"
-    breaker.turns = breaker.max_turns
-    win = true
-  else
-    if breaker.turns == breaker.max_turns
-      puts "#{breaker.max_turns - breaker.turns} guesses remain"
-      puts "Better luck next time!"
-    else
-      puts "#{breaker.max_turns - breaker.turns} guesses remain"
-    end
-  end
-end
-
-def turn(breaker,arr,f_arr, ans, win,ai)
-  if ai == true
-    current = breaker.set_row(f_arr,arr)
-  else
-    current = set_row
-  end
-  f_arr[breaker.turns] = matches(current,ans)
-  arr[breaker.turns] = current
-  print_output(breaker,arr,f_arr)
-  breaker.turns += 1
-  check_for_win(breaker,matches(current,ans),win)
-end
 
 def new_game
   puts "Would you like to play? Y / N"
   input = gets.chomp.downcase
   if input == "y"
-    breaker = Codebreaker.new(12)
+    breaker = Codebreaker.new
     maker = Codemaker.new
-    all_guesses = []
-    all_feedback = []
-    win = false
-    ai = false
-    
+  
     puts "Press 1 to be the Codebreaker or 2 to be the Codemaker"
     input = gets.chomp
     
@@ -100,13 +16,13 @@ def new_game
       
     elsif input == "2"
       puts "Please enter code"
-      input = gets.chomp
-      maker.set_code(input)
-      ai = true
+      maker.set_code
+      breaker.ai = true
+    else
     end
     
-    until breaker.turns == breaker.max_turns
-        turn(breaker,all_guesses,all_feedback,maker.code_array,win,ai)
+    until breaker.turns >= breaker.max_turns
+        breaker.turn(maker.code)
       end
       
   elsif input == "n"
@@ -117,7 +33,6 @@ def new_game
   end
   new_game
 end
-
 
 new_game
 
--- mastermind/player.rb
+++ mastermind/player.rb
@@ -1,48 +1,137 @@
 class Codebreaker
- attr_accessor :max_turns, :turns
+ attr_accessor :turns, :code_array, :fb_array, :ai
+ attr_reader :max_turns, :allowed_colours
   
-  def initialize(max_turns, turns = 0)
-    @max_turns = max_turns
-    @turns = turns
+  def initialize
+    @max_turns = 12
+    @turns = 0
+    @allowed_colours = ["R","B","Y","G","O","P"]
+    @code_array = []
+    @fb_array = []
+    @ai = false
+    
+  end 
+  
+  def set_row(ans)
+    puts "Enter row #{@turns +1} (RGYBOP) "
+    input= gets.chomp.upcase
+    until valid_input(input)
+      input = gets.chomp.upcase
+    end
+    input = input.split("")
+    save_rows(input,ans)
   end
   
-  def set_row(f_arr,arr,allowed_colours = ["R","B","Y","G","O","P"])
+  def set_row_ai(ans)
     row = []
     n = 0
      while n < 4
         if @turns > 0
-          if f_arr[@turns -1][n] =="BM"
-            row.push(arr[@turns-1][n])
+          if @fb_array[@turns -1][n] =="BM"
+            row.push(@code_array[@turns-1][n])
           else
-            row.push(allowed_colours[rand(5)])
+            row.push(@allowed_colours[rand(5)])
           end
         else
-          row.push(allowed_colours[rand(5)])
+          row.push (@allowed_colours[rand(5)])
         end
         n += 1
       end
-    return row
+    save_rows(row,ans)
   end
   
-end
-
-
-class Codemaker
-  attr_accessor :code, :code_array
-  allowed_colours = ["R","B","Y","G","O","P"]
-  
-  def set_code(string)
-    @code_array = string.upcase.split("")
-  end
-  
-  def random_code(allowed_colours = ["R","B","Y","G","O","P"])
+  def valid_input(input)
     n = 0
-    @code_array = ""
+    if input.length == 4
+      while n < 4
+        unless @allowed_colours.include?(input[n])
+          puts "Please enter a valid colour (RBYGOP)"
+          return false
+        end
+        n+= 1
+      end
+    else
+      puts "Please enter four colours (RBYGOP)"
+      return false
+    end
+    return true
+  end
+  
+  def save_rows(row,ans)
+    @code_array.push(row)
+    @fb_array.push(matches(row,ans))
+  end
+  
+  def matches(current_row,code_array)
+    n = 0
+    matches=[]
     while n < 4
-      @code_array += allowed_colours[rand(5)]
+      if current_row[n] == code_array[n]
+        matches[n] = "BM"
+      elsif code_array.include?(current_row[n]) 
+        matches[n] = "CM"
+      else 
+        matches[n] = "  "
+      end
       n += 1
     end
-    return @code_array
+    return matches
+  end
+    
+  def print_output
+    t = 0
+    while t < @turns + 1
+      puts "#{@code_array[t]} #{@fb_array[t]}"
+      t += 1
+    end
+  end
+  
+  def check_for_win
+    if @fb_array[@turns-1].all? {|x| x =="BM"}
+      puts "You win! You took #{@turns} attempts"
+      @turns = @max_turns
+    else
+      if @turns  == @max_turns
+        puts "#{@max_turns - @turns } guesses remain"
+        puts "Better luck next time!"
+      else
+        puts "#{@max_turns - @turns  } guesses remain"
+      end
+    end
+  end
+
+  def turn(ans)
+    if @ai == true
+      set_row_ai(ans)
+    else
+      set_row(ans)
+    end
+    print_output
+    @turns += 1
+    check_for_win
+  end
+end
+
+class Codemaker
+  
+  attr_reader:code, :allowed_colours
+  
+  def initialize
+   @allowed_colours = ["R","B","Y","G","O","P"]
+  end
+  
+  def set_code
+    @code = gets.chomp.upcase.split("")
+  end
+  
+  def random_code
+    n = 0
+    @code = ""
+    while n < 4
+      @code += @allowed_colours[rand(5)]
+      n += 1
+    end
+    return @code
   end
     
 end
