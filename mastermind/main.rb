ce117537abe408d2e2a66fda05ea2ecea142fa7e
Jason Dancocks <jason.dancocks@gmail.com>
2017-07-14 18:05:44 +0100

    Final version -
    Refactored code into much more defined methods, abstracts main turn  to a class. added code comments.

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -1,38 +1,51 @@
 require_relative "player"
-
-
-def new_game
-  puts "Would you like to play? Y / N"
-  input = gets.chomp.downcase
-  if input == "y"
-    breaker = Codebreaker.new
-    maker = Codemaker.new
+class Mastermind
+  attr_accessor :breaker, :maker
+  # creates new instance and starts game
+  def initialize
+    puts "Welcome to Mastermind!" 
+    start
+  end
   
+  #starts new game process, allows user to exit
+  def start
+    puts "Press Y to play or N to exit"
+    input = gets.chomp.downcase
+      if input == "y"
+        new_game
+      elsif input == "n"
+        puts "Exiting..."
+      else
+        start
+      end
+  end
+  
+  #choose to which player to be and calls the codemaker to set the code.
+  def choose_role
     puts "Press 1 to be the Codebreaker or 2 to be the Codemaker"
     input = gets.chomp
-    
     if input == "1"
-      maker.random_code
-      
+      @maker.random_code
     elsif input == "2"
-      puts "Please enter code"
-      maker.set_code
-      breaker.ai = true
+      @maker.set_code
+      @breaker.ai = true
     else
+      choose_role
     end
-    
-    until breaker.turns >= breaker.max_turns
-        breaker.turn(maker.code)
-      end
-      
-  elsif input == "n"
-    puts "Exiting..."
-    return false
-  else
-      new_game
   end
-  new_game
+  
+  #starts a new game, initializes both players and loops the turns.
+  def new_game
+    @breaker = Codebreaker.new
+    @maker = Codemaker.new
+    choose_role
+    until @breaker.turns >= @breaker.max_turns
+      @breaker.turn(@maker.code)
+      @breaker.turns
+    end
+    puts "Play again?"
+    start
+  end
 end
 
-new_game
-
+Mastermind.new
--- mastermind/player.rb
+++ mastermind/player.rb
@@ -1,7 +1,41 @@
-class Codebreaker
+#Methods to get and valididate input to match allowed colours
+class Player
+   
+   # retrieves input and runs validation
+   def get_input
+    input= gets.chomp.upcase
+    until valid_input(input)
+      input = gets.chomp.upcase
+    end
+    return input
+  end
+  
+  #validates input by comparing it to allowed colours
+   def valid_input(input)
+    n = 0
+    if input.length == 4
+      while n < 4
+        unless @allowed_colours.include?(input[n])
+          puts "Please enter a valid colour (RBYGOP)"
+          return false
+        end
+        n+= 1
+      end
+    else
+      puts "Please enter four colours (RBYGOP)"
+      return false
+    end
+    return true
+  end
+end
+
+#contains methods for the codebreaker player
+class Codebreaker < Player
  attr_accessor :turns, :code_array, :fb_array, :ai
  attr_reader :max_turns, :allowed_colours
   
+  #sets turn counter, max turns and sets the allowed colours list
+  #also sets up the arrays to store the codes and the matches
   def initialize
     @max_turns = 12
     @turns = 0
@@ -9,19 +43,15 @@ class Codebreaker
     @code_array = []
     @fb_array = []
     @ai = false
-    
   end 
   
+  #human version of the set row method - sets reuqired move by player
   def set_row(ans)
     puts "Enter row #{@turns +1} (RGYBOP) "
-    input= gets.chomp.upcase
-    until valid_input(input)
-      input = gets.chomp.upcase
-    end
-    input = input.split("")
-    save_rows(input,ans)
+    save_rows(get_input.split(""),ans)
   end
   
+  #ai version of the set row method, randomly generates are code
   def set_row_ai(ans)
     row = []
     n = 0
@@ -39,29 +69,15 @@ class Codebreaker
       end
     save_rows(row,ans)
   end
-  
-  def valid_input(input)
-    n = 0
-    if input.length == 4
-      while n < 4
-        unless @allowed_colours.include?(input[n])
-          puts "Please enter a valid colour (RBYGOP)"
-          return false
-        end
-        n+= 1
-      end
-    else
-      puts "Please enter four colours (RBYGOP)"
-      return false
-    end
-    return true
-  end
-  
+
+  #saves the entered moves to the code array
   def save_rows(row,ans)
     @code_array.push(row)
     @fb_array.push(matches(row,ans))
   end
   
+  #checks for matches, allocates "BM" for both matching and "CM" for colour matches
+
   def matches(current_row,code_array)
     n = 0
     matches=[]
@@ -77,7 +93,8 @@ class Codebreaker
     end
     return matches
   end
-    
+  
+  # prints entire gameboard and corresponding matches each turn  
   def print_output
     t = 0
     while t < @turns + 1
@@ -86,20 +103,30 @@ class Codebreaker
     end
   end
   
+  #checks after each turn to see if all match, with different outputs depending on the player
   def check_for_win
     if @fb_array[@turns-1].all? {|x| x =="BM"}
-      puts "You win! You took #{@turns} attempts"
-      @turns = @max_turns
+      if @ai
+        puts "You lose! The computer cracked your code in #{@turns} attempts"
+      else
+        puts "You win! You took #{@turns} attempts"
+      end
+        @turns = @max_turns
     else
       if @turns  == @max_turns
         puts "#{@max_turns - @turns } guesses remain"
-        puts "Better luck next time!"
+        if @ai
+          puts "You win! The computer failed to crack your code"
+        else
+          puts "Better luck next time!"
+        end
       else
         puts "#{@max_turns - @turns  } guesses remain"
       end
     end
   end
 
+  # takes the input, saves the row, displays the gameboard and checks for a winning input.
   def turn(ans)
     if @ai == true
       set_row_ai(ans)
@@ -112,18 +139,21 @@ class Codebreaker
   end
 end
 
-class Codemaker
-  
+#methods for the codemaker
+class Codemaker < Player
   attr_reader:code, :allowed_colours
   
+  #sets alllowed colours
   def initialize
    @allowed_colours = ["R","B","Y","G","O","P"]
   end
-  
+  # takes code from human player
   def set_code
-    @code = gets.chomp.upcase.split("")
+    puts "Please enter code"
+    @code = get_input.split("")
   end
   
+  #generates a random valid code
   def random_code
     n = 0
     @code = ""
